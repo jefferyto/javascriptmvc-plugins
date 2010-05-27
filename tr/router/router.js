5e41c36d0027e34fdcaba1047e6e3ced64cf547b
Thomas Reynolds <tdreyno@gmail.com>
2010-05-27 14:23:18 -0700

    simplify router

--- tr/router/router.js
+++ tr/router/router.js
@@ -1,5 +1,89 @@
 steal.then("uniform_string_prototype_split")
      .then("sherpa")
      .then(function($) {
-       Router = new Sherpa.Router();
-     });
+
+if (steal.options.env == 'production') {
+  steal.dev = steal.dev || { log: function(){} };
+}
+
+Router = new Sherpa.Router();
+
+$.Class.extend("TR.Router",
+{
+  currentParams: {},
+  URLPrefix:     null,
+  
+  init: function() {
+    OpenAjax.hub.subscribe("history.**", function(event_name) {
+      //You'll also typically need to match the "index" history, which typically
+      //occurs when no specific url has been entered.  You can route to your
+      //default url here. 
+      if(event_name === 'history.index') {
+        OpenAjax.hub.publish("router.index_page");
+        return;
+      }
+
+      var key = event_name.replace(/^history(\.*)/, '/');
+      //steal.dev.log('History event: ' + key);
+
+      var foundRoute = Router.recognize(key);
+      if (!foundRoute) {
+        steal.dev.log("WARNING: Router failed to find route for: " + key);
+        return;
+      }
+      
+      this.currentParams = foundRoute.params;
+      OpenAjax.hub.publish(foundRoute.destination, foundRoute.params);
+    });
+  },
+  
+  /**
+   * Given an html uri, match it to a history point and update the history
+   * of the page to navigate to that location.
+   */
+  routeTo: function(uri) {
+    //steal.dev.log("ROUTER: Request to route to " + uri);
+
+    //Remove /dev.php from the uri;
+    if (this.Class.URLPrefix) {
+      var r = new RegExp(this.Class.URLPrefix);
+      uri = uri.replace(r, '');
+    }
+    
+    var uriWithHash = "#" + uri;
+
+    if (window.location.hash === uriWithHash) {
+      steal.dev.log("ROUTER: Already at " + uriWithHash);
+      return;
+    }
+    
+    var route = Router.recognize(uri);
+    if (!route) {
+      //No route.  Let it go.  Add a '#' to the end so that we don't infinite redirect.
+      window.location.href = uri + '#';
+      return;
+    } else {
+      //steal.dev.log("ROUTER: Detected appropriate history point: " + uriWithHash);
+    }
+
+    var components = window.location.href.split('#');
+    if (components.length > 1) {
+      steal.dev.log('ROUTER: ' + window.location.hash + ' -> ' + uriWithHash);
+      if (window.location.hash !== uriWithHash) {
+        window.location = components[0] + uriWithHash;
+        return true;
+      } else {
+        return false;
+      }
+    } else { //let's "redirect" to the appropriate javascriptmvc uri
+      var cl = window.location;
+      var prefix = this.Class.URLPrefix || '';
+      window.location = cl.protocol + '//' + cl.hostname + prefix + uriWithHash;
+    }
+  }
+},
+{
+}
+);
+
+});
--- tr/router/test/qunit/qunit.js
+++ tr/router/test/qunit/qunit.js
@@ -2,4 +2,4 @@ steal
   .plugins("tr/router") //load your app
   .plugins("funcunit/qunit")     //load qunit
   .then("sherpa_test")
-  .then("router_controller_test")
+  .then("router_test")
